07dfecb9aac5b6625a47e9db68f00de06eeef492
emerjayd <mrj.d@mail.com>
2024-07-18 10:33:32 +0100

    5

--- s3_dynamodb.tf
+++ s3_dynamodb.tf
@@ -1,33 +1,33 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket"
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "terraform-state-bucket"
 
-  lifecycle {
-    prevent_destroy = true
-  }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
 
-  tags = {
-    Name        = "terraform-state-bucket"
-    Environment = "dev"
-  }
-}
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+#   tags = {
+#     Name        = "terraform-state-bucket"
+#     Environment = "dev"
+#   }
+# }
+# resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
 
-  tags = {
-    Name        = "terraform-locks"
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Name        = "terraform-locks"
+#     Environment = "dev"
+#   }
+# }
