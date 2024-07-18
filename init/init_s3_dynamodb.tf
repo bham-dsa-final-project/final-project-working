91bcd996e96ba256f472d6b7d4b2f5dacf3b0de7
emerjayd <mrj.d@mail.com>
2024-07-18 10:45:37 +0100

    2

--- init/init_s3_dynamodb.tf
+++ init/init_s3_dynamodb.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket"
+  bucket = "terraform-state-bucket2"
 
   lifecycle {
     prevent_destroy = true
   }
 
   tags = {
-    Name        = "terraform-state-bucket"
+    Name        = "terraform-state-bucket2"
     Environment = "dev"
   }
 }
