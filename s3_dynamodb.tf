5080bdb192801021f1ab29145fa485a11960b347
emerjayd <mrj.d@mail.com>
2024-07-18 10:02:08 +0100

    1/18.07.24

---
+++ s3_dynamodb.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform_state_bucket"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name        = "terraform-state-bucket"
+    Environment = "dev"
+  }
+}
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "terraform-locks"
+    Environment = "dev"
+  }
+}
